e96fea063e71b3448db557c502094cfa0fd1750e
adonixis <adonixis@gmail.com>
2017-01-03 01:16:32 +0500

    Initial commit

---
+++ app/src/main/java/ru/adonixis/animatedmenuitems/MainActivity.java
@@ -0,0 +1,58 @@
+package ru.adonixis.animatedmenuitems;
+
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Menu menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("Anim Items");
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (menu != null) {
+                    for (int i = 0; i < menu.size(); i++) {
+                        Drawable drawable = menu.getItem(i).getIcon();
+                        if (drawable instanceof Animatable) {
+                            ((Animatable) drawable).start();
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        this.menu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Drawable drawable = item.getIcon();
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
